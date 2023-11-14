c7eec2ac23389f5cc747998ba6dfc999b1ae6ca0
beasroy <roybipasha676@gmail.com>
2023-11-14 15:28:03 +0530

    width change

--- client/src/pages/Model.jsx
+++ client/src/pages/Model.jsx
@@ -12,7 +12,7 @@ const Model = () => {
       <div className='h-64'> */}
        <img className='w-[50vh] h-[70vh]'src={Download} />
        <a className='text-xl text-center mt-5 w-full' href='https://drive.google.com/drive/folders/1AiQGz9xkFX16s8WLJYuXsIkqzYvytD_Q?usp=sharing'>
-       <button  className="rounded-lg w-1/3 h-12 mb-4 mx-auto bg-gradient-to-tr from-rose-500 to-[#3D0C11]  text-white text-xl hover:scale-105 hover:shadow-md hover:shadow-red-800/60">Download Now</button>
+       <button  className="rounded-lg w-full md:w-1/3 h-12 mb-4 mx-auto bg-gradient-to-tr from-rose-500 to-[#3D0C11]  text-white text-xl hover:scale-105 hover:shadow-md hover:shadow-red-800/60">Download Now</button>
      </a>
       </div>
      
